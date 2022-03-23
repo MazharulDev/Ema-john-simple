d73867f71aac411eb04336e93c7b68c1d3553153
Mazharul islam <miforbd@gmail.com>
2022-03-23 09:22:17 +0600

    header and product and cart simple added

---
+++ src/component/Product/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+const Product = (props) => {
+    const {img,name,ratings,price,seller}=props.product
+    return (
+        <div className='product'>
+            <img src={img} alt=''/>
+            <div className='product-info'>
+            <p className='product-name'>{name}</p>
+            <p>Price: ${price}</p>
+            <p><small>Seller: {seller}</small></p>
+            <p><small>Ratings: {ratings}</small></p>
+            </div>
+            <button onClick={()=>props.handleAddToCart(props.product)} className='btn-cart'>
+                <p>Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
